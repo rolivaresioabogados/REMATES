3f89dd585d03b985e999dd3f78ee18768b22375b
rolivaresioabogados <rolivares@ioabogados.cl>
2026-05-04 09:40:00 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // Service Worker para PWA Remates - con auto-update
 // La versión se incrementa con cada cambio importante. Cambiar este número
 // fuerza la actualización de todos los clientes.
-const VERSION = 'v7';
+const VERSION = 'v9';
 const CACHE_NAME = 'remates-' + VERSION;
 
 // Solo cachear assets estáticos que cambian poco
